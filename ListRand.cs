0b1f41704a22e3b543ca699b9707c1d19f80e72f
BokarevDanila <danila228337228@gmail.com>
2023-03-07 22:52:53 +0300

    Serializing and deserializing

---
+++ ListRand.cs
@@ -0,0 +1,99 @@
+﻿public class ListRand
+{
+    public ListNode Head;
+    public ListNode Tail;
+    public int Count;
+
+    private ListNode GetNode(int index)
+    {
+        int counter = 0;
+        for (ListNode currentNode = Head; currentNode != null; currentNode = currentNode.Next)
+        {
+            if (counter == index)
+                return currentNode;
+            counter++;
+        }
+        return new ListNode();
+    }
+
+    public void Serialize(FileStream s)
+    {
+        Dictionary<ListNode, int> randomNodeIndexes = new Dictionary<ListNode, int>();
+        int indexCounter = 0;
+        Head.Rand = null;
+
+        for (ListNode currentNode = Head; currentNode != null; currentNode = currentNode.Next)
+        {
+            if (currentNode.Rand != null)
+                randomNodeIndexes.Add(currentNode, indexCounter);
+            else
+                randomNodeIndexes.Add(currentNode, -1);
+            indexCounter++;
+        }
+
+        using (StreamWriter streamReader = new StreamWriter(s))
+        {
+            for (ListNode currentNode = Head; currentNode != null; currentNode = currentNode.Next)
+            {
+                string result = currentNode.Data + ":";
+                if (currentNode.Rand != null)
+                {
+                    streamReader.Write(result + randomNodeIndexes[currentNode.Rand] + ";");
+                }
+                else
+                {
+                    streamReader.Write(result + "-1;");
+                }
+            }
+        }
+    }
+
+    public void Deserialize(FileStream s)
+    {
+        try
+        {
+            List<int> randomNodeIndexes = new List<int>();
+            string[] splittedText = new StreamReader(s).ReadToEnd().Split(";");
+            ListNode currentNode = new ListNode();
+
+            currentNode = new ListNode();
+            string text = splittedText[0];
+            var values = text.Split(":");
+            var data = values[0];
+            int index = int.Parse(values[1]);
+            randomNodeIndexes.Add(index);
+            currentNode.Data = data;
+
+            Head = currentNode;
+
+            for (int i = 1; i < splittedText.Length - 1; i++)
+            {
+                text = splittedText[i];
+                values = text.Split(":");
+                data = values[0];
+                index = int.Parse(values[1]);
+
+                var node = new ListNode();
+                currentNode.Next = node;
+                node.Data = data;
+                node.Prev = currentNode;
+                currentNode = node;
+                currentNode.Data = data;
+                randomNodeIndexes.Add(index);
+            }
+
+            Tail = currentNode;
+
+            for (int i = 0; i < randomNodeIndexes.Count; i++)
+            {
+                if (randomNodeIndexes[i] != -1)
+                    GetNode(i).Rand = GetNode(randomNodeIndexes[i]);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Не удалось считать файл:");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
